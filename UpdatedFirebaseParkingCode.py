cd71555ad6cbe1a9cf87f6eeca1d3c3dce721179
Shreyans Jain <35236360+Chemdevil@users.noreply.github.com>
2022-04-06 18:23:39 +0530

    Update UpdatedFirebaseParkingCode.py

--- UpdatedFirebaseParkingCode.py
+++ UpdatedFirebaseParkingCode.py
@@ -4,14 +4,14 @@ import random
 import pyrebase
 
 config={
-    "apiKey": "AIzaSyAZYM5i0zhsjxB9uUoWsFEL28VSpxsoTIo",
-  "authDomain": "smartparkingsensor-group7.firebaseapp.com",
-  "databaseURL": "https://smartparkingsensor-group7-default-rtdb.firebaseio.com",
-  "projectId": "smartparkingsensor-group7",
-  "storageBucket": "smartparkingsensor-group7.appspot.com",
-  "messagingSenderId": "810478901892",
-  "appId": "1:810478901892:web:abffb8aa6ec2f98729c0d1",
-  "measurementId": "G-G4D2SXJ92Z"
+    "apiKey": "",
+  "authDomain": "",
+  "databaseURL": "",
+  "projectId": "",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "",
+  "measurementId": ""
     };
 
 def distance(GPIO_TRIG,GPIO_ECHO):        
